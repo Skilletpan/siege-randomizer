3619110a2f1147628b4e4ef7f0dd57ca24f2a264
Skilletpan <skilletpan.14@gmail.com>
2023-08-12 02:56:14 +0200

    Map card improvements

--- src/components/MapCard.vue
+++ src/components/MapCard.vue
@@ -1,8 +1,10 @@
 <template>
   <v-card variant="tonal" :width="big ? 500 : 300">
-    <v-img :alt="value.name" :aspect-ratio="16 / 9" :class="{ inactive }" :lazy-src="require('@/assets/maps/OREGON.jpg')"
-      :src="image" />
-    <v-card-title :class="{ 'text-button': isEmptyRandomize, 'text-center': true, 'text-primary': isEmptyRandomize }">
+    <!-- Map Image -->
+    <v-img :alt="value.name" :aspect-ratio="16 / 9" :class="{ inactive }" :lazy-src="PLACEHOLDER_MAP" :src="image" />
+
+    <!-- Map Name -->
+    <v-card-title :class="['text-center', { 'text-button': isEmptyRandomize, 'text-primary': isEmptyRandomize }]">
       {{ value.name }}
     </v-card-title>
   </v-card>
@@ -10,6 +12,9 @@
 
 <script setup>
 import { computed, defineProps } from 'vue';
+
+// Define static properties
+const PLACEHOLDER_MAP = require('@/assets/maps/OREGON.jpg');
 
 // Define input properties
 const props = defineProps({
@@ -29,7 +34,7 @@ const props = defineProps({
 
 // Define computed properties
 const image = computed(() => props.value.key ? require(`@/assets/maps/${props.value.key}.jpg`) : null);
-const isEmptyRandomize = computed(() => !props.value.key && props.big);
+const isEmptyRandomize = computed(() => props.big && !props.value.key);
 </script>
 
 <style scoped>
