e4423646c54b50f569465195233fb6f4c65373a2
Skilletpan <skilletpan.14@gmail.com>
2023-12-08 08:01:23 +0100

    Added missing index changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import MapCard from './MapCard';
 import MatchDrawer from './MatchDrawer';
 import NavigationDrawer from './NavigationDrawer';
-import OperatorCard from './OperatorCard';
-import OperatorCard2 from './operators/OperatorCard';
+import OperatorCard from './operators/OperatorCard';
 import OperatorFilterDrawer from './OperatorFilterDrawer';
 import OperatorLabel from './OperatorLabel';
 import OperatorSearch from './OperatorSearch';
+import SidePool from './SidePool';
 import StratCard from './StratCard';
 import StratFilterDrawer from './StratFilterDrawer';
 
@@ -14,10 +14,10 @@ export {
   MatchDrawer,
   NavigationDrawer,
   OperatorCard,
-  OperatorCard2,
   OperatorFilterDrawer,
   OperatorLabel,
   OperatorSearch,
+  SidePool,
   StratCard,
   StratFilterDrawer
 };
