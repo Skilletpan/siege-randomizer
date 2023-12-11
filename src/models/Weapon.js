a4e486625cd4c6bad99ce852a7a078ada74095ef
Skilletpan <skilletpan.14@gmail.com>
2023-12-11 09:00:24 +0100

    Added weapon slot information

--- src/models/Weapon.js
+++ src/models/Weapon.js
@@ -8,7 +8,7 @@ export default class Weapon extends MapModel {
     // Build weapon instances from raw data
     Object.entries(rawWeapons).forEach(([key, weapon]) => {
       // Create weapon instance
-      new Weapon({ key, name: weapon });
+      new Weapon({ key, ...weapon });
     });
 
     console.debug('Weapons imported:', Weapon.LIST);
@@ -28,21 +28,30 @@ export default class Weapon extends MapModel {
 
   // Instance properties
   #name;
+  #slot;
 
   /**
    * Creates a new Weapon instance.
    * 
-   * @param {Object} rawWeapon      The raw weapon data.
-   * @param {string} rawWeapon.key  The key of the weapon.
-   * @param {string} rawWeapon.name The name of the weapon.
+   * @param {Object}                 rawWeapon      The raw weapon data.
+   * @param {string}                 rawWeapon.key  The key of the weapon.
+   * @param {string}                 rawWeapon.name The name of the weapon.
+   * @param {?'primary'|'secondary'} rawWeapon.slot The weapon slot the weapon belongs in.
    */
   constructor(rawWeapon) {
     super(rawWeapon.key, Weapon);
 
     // Set instance properties
     this.#name = rawWeapon.name;
+    this.#slot = rawWeapon.slot;
   }
 
   /** @returns {string} The name of the weapon. */
   get name() { return this.#name; }
+
+  /** @returns {boolean} Whether the weapon is a primary weapon. */
+  get isPrimary() { return !this.#slot || this.#slot === 'primary'; }
+
+  /** @returns {boolean} Whether the weapon is a secondary weapon. */
+  get isSecondary() { return !this.#slot || this.#slot === 'secondary'; }
 }
