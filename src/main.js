1dbd5fc06f037880988943eecfe96378ccbe72fb
Skilletpan <skilletpan.14@gmail.com>
2023-12-15 07:15:35 +0100

    Added forgotten pinia import

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-loadFonts()
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
+
+loadFonts();
 
 createApp(App)
+  .use(createPinia())
   .use(router)
   .use(vuetify)
-  .mount('#app')
+  .mount('#app');
