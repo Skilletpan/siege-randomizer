5d831fa52ecfc629572a21f6e32eafaab4468816
Skilletpan <skilletpan.14@gmail.com>
2024-01-15 17:30:30 +0100

    Updated Vue config

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,15 @@ module.exports = defineConfig({
   publicPath: '/siege-randomizer',
   transpileDependencies: true,
 
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions;
+    });
+  },
+
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
