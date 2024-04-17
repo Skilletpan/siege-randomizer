361a660a30b923e37238fb2355e5bd41e6a64867
Skilletpan <skilletpan.14@gmail.com>
2024-04-17 06:38:33 +0200

    Moved Player Input to separate component

---
+++ src/components/teams/PlayerInput.vue
@@ -0,0 +1,89 @@
+<template>
+  <!-- Player Name Input -->
+  <v-autocomplete
+    v-model="input.value"
+    v-model:search="input.search"
+    clear-on-select
+    :items="inputItems"
+    label="Add Player"
+    multiple
+    no-data-text="No players"
+    placeholder="Player name"
+    @keyup.enter="addPlayer(input.search)"
+    @update:model-value="addPlayer($event[0])"
+  >
+    <template #item="{ item, props }">
+      <!-- Autocomplete List Item -->
+      <v-list-item
+        v-bind="props"
+        prepend-icon="mdi-history"
+      >
+        <template #append>
+          <!-- Item Remove Button -->
+          <v-list-item-action end>
+            <v-btn
+              density="comfortable"
+              icon="$delete"
+              variant="plain"
+              @click.stop="Players.removePlayer(item.value)"
+            />
+          </v-list-item-action>
+        </template>
+      </v-list-item>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+import { usePlayers } from '@/stores';
+
+// Register composables
+const Players = usePlayers();
+
+/**
+ * The list to add player names to.
+ * @type {import('vue').ModelRef<String[]>}
+ */
+const players = defineModel({ type: Array, default: [] });
+
+// Component props
+const props = defineProps({
+  /** The player name items to pick from.  */
+  items: Array
+});
+
+/**
+ * The input values.
+ * @type {import('vue').Ref<{ value: string[], search: string }>}
+ */
+const input = ref({
+  value: [],
+  search: null
+});
+
+/**
+ * The player name items to choose from.
+ * @type {import('vue').ComputedRef<String[]>}
+ */
+const inputItems = computed(() => props.items || Players.storedPlayers);
+
+/**
+ * Adds a player name to the list.
+ * 
+ * @param {string} playerName The player name to add.
+ */
+function addPlayer(playerName) {
+  // Only add valid player names
+  if (playerName?.length >= 3 && !players.value.includes(playerName)) {
+    // Add player names to list and store
+    players.value.push(playerName);
+    Players.addPlayer(playerName);
+
+    // Reset input values
+    input.value.value.length = 0;
+    input.value.search = null;
+  }
+}
+</script>
--- src/components/teams/SquadMenu.vue
+++ src/components/teams/SquadMenu.vue
@@ -47,78 +47,27 @@
         v-if="Players.currentPlayers.length < 5"
         class="mt-2"
       >
-        <v-autocomplete
-          v-model="playerInput.value"
-          v-model:search="playerInput.search"
-          clear-on-select
+        <player-input
+          v-model="Players.currentPlayers"
           :items="recentPlayers"
-          label="Add Player"
-          multiple
-          no-data-text="No players"
-          placeholder="Player Name"
-          @keyup.enter="addPlayer(playerInput.search)"
-          @update:model-value="addPlayer($event[0])"
-        >
-          <template #item="{ item, props }">
-            <v-list-item
-              v-bind="props"
-              prepend-icon="mdi-history"
-            >
-              <template #append>
-                <v-list-item-action end>
-                  <v-btn
-                    density="comfortable"
-                    icon="$delete"
-                    variant="flat"
-                    @click.stop="Players.removePlayer(item.value)"
-                  />
-                </v-list-item-action>
-              </template>
-            </v-list-item>
-          </template>
-        </v-autocomplete>
+        />
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 
+import { PlayerInput } from '@/components';
 import { usePlayers } from '@/stores';
 
 // Register composables
 const Players = usePlayers();
 
 /**
- * The current player input values.
- * @type {import('vue').Ref<{ search: string, value: string[] }>}
- */
-const playerInput = ref({
-  search: null,
-  value: []
-});
-
-/**
  * The list of stored players with current players filtered out.
  * @type {import('vue').ComputedRef<String[]>}
  */
 const recentPlayers = computed(() => Players.storedPlayers.filter((p) => !Players.currentPlayers.includes(p)));
-
-/**
- * Adds a player name to the `currentPlayers` list.
- * 
- * @param {string} playerName The player name to add.
- */
-function addPlayer(playerName) {
-  if (playerName?.length >= 3 && !Players.currentPlayers.includes(playerName)) {
-    // Add player name to list and storage
-    Players.currentPlayers.push(playerName);
-    Players.addPlayer(playerName);
-
-    // Reset player name input
-    playerInput.value.value.length = 0;
-    playerInput.value.search = null;
-  }
-}
 </script>
