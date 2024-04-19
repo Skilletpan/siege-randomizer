adba75924fff0ddff0a9047598191524ef319874
Skilletpan <skilletpan.14@gmail.com>
2024-04-19 03:35:22 +0200

    Mobile friendly team picker

--- src/views/TeamPicker.vue
+++ src/views/TeamPicker.vue
@@ -1,56 +1,115 @@
 <template>
   <v-container class="py-6 py-sm-8 py-md-10 py-lg-12">
     <v-row justify="center">
+      <template v-if="smAndUp">
+        <v-col
+          v-for="side, index in Side.LIST"
+          :key="side.key"
+          cols="5"
+          lg="4"
+          :order="index"
+        >
+          <!-- Team Cards -->
+          <v-card class="pb-1">
+            <v-toolbar
+              class="mb-1"
+              :color="AppSettings.colors[side.key]"
+              density="compact"
+              elevation="1"
+              :title="side.name"
+            >
+              <v-toolbar-items class="pr-1">
+                <!-- Set as Squad Button -->
+                <v-btn
+                  :disabled="teams[index].length === 0"
+                  icon
+                  @click="Players.currentPlayers = Array.from(teams[index])"
+                >
+                  <v-icon icon="mdi-account-group" />
+
+                  <v-tooltip
+                    activator="parent"
+                    location="left"
+                    text="Set as Current Squad"
+                  />
+                </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+
+            <!-- Player Item -->
+            <v-card-item
+              v-for="playerName, index2 in teams[index]"
+              :key="playerName"
+              :append-icon="index2 === 0 ? 'mdi-crown' : null"
+            >
+              {{ playerName }}
+            </v-card-item>
+
+            <!-- Empty Slot Item -->
+            <v-card-item
+              v-for="index in (5 - teams[index].length)"
+              :key="index"
+              class="font-italic text-grey"
+            >
+              Empty
+            </v-card-item>
+          </v-card>
+        </v-col>
+      </template>
+
       <v-col
-        v-for="side, index in Side.LIST"
-        :key="side.key"
-        cols="3"
-        :order="index"
+        v-else
+        cols="12"
       >
-        <!-- Team Cards -->
+        <!-- Teams Card -->
         <v-card class="pb-1">
           <v-toolbar
             class="mb-1"
-            :color="AppSettings.colors[side.key]"
+            :color="AppSettings.colors[teamTab]"
             density="compact"
             elevation="1"
-            :title="side.name"
           >
-            <v-toolbar-items class="pr-1">
-              <!-- Set as Squad Button -->
-              <v-btn
-                :disabled="teams[index].length === 0"
-                icon
-                @click="Players.currentPlayers = Array.from(teams[index])"
-              >
-                <v-icon icon="mdi-account-group" />
-
-                <v-tooltip
-                  activator="parent"
-                  location="left"
-                  text="Set as Current Squad"
-                />
-              </v-btn>
-            </v-toolbar-items>
+            <!-- Side Tabs -->
+            <v-tabs
+              v-model="teamTab"
+              fixed-tabs
+              grow
+            >
+              <v-tab
+                v-for="side in Side.LIST"
+                :key="side.key"
+                :text="side.name"
+                :value="side.key"
+              />
+            </v-tabs>
           </v-toolbar>
 
-          <!-- Player Item -->
-          <v-card-item
-            v-for="playerName, index2 in teams[index]"
-            :key="playerName"
-            :append-icon="index2 === 0 ? 'mdi-crown' : null"
-          >
-            {{ playerName }}
-          </v-card-item>
+          <v-window v-model="teamTab">
+            <!-- Team Lists -->
+            <v-window-item
+              v-for="side, index in Side.LIST"
+              :key="side.key"
+              :value="side.key"
+            >
+              <!-- Player Item -->
+              <v-card-item
+                v-for="playerName, index2 in teams[index]"
+                :key="playerName"
+                :append-icon="index2 === 0 ? 'mdi-crown' : null"
+              >
+                {{ playerName }}
+              </v-card-item>
 
-          <!-- Empty Slot Item -->
-          <v-card-item
-            v-for="index in (5 - teams[index].length)"
-            :key="index"
-            class="font-italic text-grey"
-          >
-            Empty
-          </v-card-item>
+              <!-- Empty Slot Item -->
+              <v-card-item
+                v-for="index in (5 - teams[index].length)"
+                :key="index"
+                class="font-italic text-grey"
+              >
+                Empty
+              </v-card-item>
+            </v-window-item>
+          </v-window>
         </v-card>
       </v-col>
 
@@ -70,7 +129,12 @@
     </v-row>
 
     <v-row justify="center">
-      <v-col cols="4">
+      <v-col
+        cols="12"
+        sm="6"
+        md="5"
+        lg="4"
+      >
         <!-- Player Names Input -->
         <player-input
           v-model="playerNames"
@@ -95,13 +159,15 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, shallowRef } from 'vue';
+import { useDisplay } from 'vuetify';
 
 import { PlayerInput } from '@/components';
 import { Side } from '@/models';
 import { useAppSettings, usePlayers } from '@/stores';
 
 // Register composables
+const { smAndUp } = useDisplay();
 const AppSettings = useAppSettings();
 const Players = usePlayers();
 
@@ -116,6 +182,12 @@ const playerNames = ref([]);
  * @type {import('vue').Ref<String[][]>}
  */
 const teams = ref([[], []]);
+
+/**
+ * The side tab to show.
+ * @type {import('vue').ShallowRef<String>}
+ */
+const teamTab = shallowRef(Side.ATT.key);
 
 /** Builds two teams from the entered player names. */
 function randomizeTeams() {
