4ae36646af4ab446d18d1d2e79fce7e3c9ae377f
Skilletpan <skilletpan.14@gmail.com>
2024-06-19 05:23:30 +0200

    Fixed Operator Card paddings

--- src/components/operators/OperatorCard.vue
+++ src/components/operators/OperatorCard.vue
@@ -62,9 +62,9 @@
         <!-- Operator Details Tab -->
         <v-window-item :value="DETAIL_TABS[0]">
           <!-- Speed and Health -->
-          <v-card-item>
+          <v-card-item class="px-4 py-3">
             <v-label
-              class="d-block mb-1 text-caption"
+              class="d-block mb-1 mt-n1 text-caption"
               text="Speed and Health"
             />
             <v-chip
@@ -81,9 +81,9 @@
           </v-card-item>
 
           <!-- Roles -->
-          <v-card-item class="pt-1">
+          <v-card-item class="pb-3 pt-1 px-4">
             <v-label
-              class="d-block mb-1 text-caption"
+              class="d-block mb-1 mt-n1 text-caption"
               text="Roles"
             />
             <div class="ma-n1">
@@ -101,10 +101,10 @@
           <!-- Squad -->
           <v-card-item
             v-if="operator.squad"
-            class="pt-1"
+            class="pb-3 pt-1 px-4"
           >
             <v-label
-              class="d-block text-caption"
+              class="d-block mt-n1 text-caption"
               text="Squad"
             />
 
@@ -126,10 +126,11 @@
           <v-card-item
             v-for="index in 3"
             :key="index"
-            :class="{ 'pt-1': index > 1 }"
+            class="px-4"
+            :class="index === 1 ? 'py-3' : 'pb-3 pt-1'"
           >
             <v-label
-              class="d-block mb-1 text-caption"
+              class="d-block mb-1 mt-n1 text-caption"
               :text="['Primary Weapons', 'Secondary Weapons', 'Gadgets'][index - 1]"
             />
             <div class="ma-n1">
