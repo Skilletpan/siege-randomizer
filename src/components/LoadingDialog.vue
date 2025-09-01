379055b428fb9208626383a46ff5697dc18c94d9
Skilletpan <skilletpan.14@gmail.com>
2025-09-01 21:41:08 +0200

    Added loading dialog framework

---
+++ src/components/LoadingDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-dialog
+    v-model="DialogStore.isLoading"
+    persistent
+    width="300"
+  >
+    <v-card :title="DialogStore.dialogTitle">
+      <v-card-text>
+        <!-- Loader -->
+        <v-progress-linear
+          :class="{ 'mb-1': DialogStore.currentStep?.title }"
+          color="primary"
+          indeterminate
+          rounded
+          rounded-bar
+        />
+
+        <!-- Step Text -->
+        <span
+          v-if="DialogStore.currentStep?.title"
+          class="text-caption text-medium-emphasis"
+        >
+          {{ DialogStore.currentStep.title }}
+        </span>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { useDialogStore } from '@/stores';
+
+const DialogStore = useDialogStore();
+</script>
