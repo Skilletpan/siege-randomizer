79cfcffc5905b7b10784b9f673ad063782c65816
Skilletpan <skilletpan.14@gmail.com>
2025-09-02 08:26:54 +0200

    Made side-navigation work on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <v-app>
+    <!-- App Bar (mobile only) -->
+    <v-app-bar
+      v-if="mobile"
+      :title="APP_NAME"
+    >
+      <!-- Side Navigation Toggle -->
+      <template #prepend>
+        <v-btn
+          icon="mdi-menu"
+          @click="showNav = !showNav"
+        />
+      </template>
+    </v-app-bar>
+
     <!-- Side Navigation -->
-    <v-navigation-drawer>
+    <v-navigation-drawer v-model="showNav">
       <!-- Name and Version -->
       <v-list-item class="pb-4 pt-3">
         <v-list-item-title class="font-weight-bold text-h5">{{ APP_NAME }}</v-list-item-title>
@@ -28,6 +42,12 @@
 </template>
 
 <script setup lang="ts">
+import { shallowRef } from 'vue';
+import { useDisplay } from 'vuetify';
+
+// Display Breakpoints
+const { mobile } = useDisplay();
+
 // App Information
 const APP_NAME = 'Siege Randomizer';
 const APP_VERSION = import.meta.env.VITE_VERSION;
@@ -38,4 +58,7 @@ const NAV_ITEMS = [
   { type: 'divider' },
   { title: 'GitHub', props: { prependIcon: 'mdi-github', href: 'https://github.com/Skilletpan/siege-randomizer', target: '_blank' } }
 ];
+
+/** Whether the side navigation should be displayed. */
+const showNav = shallowRef<boolean>(!mobile.value);
 </script>
