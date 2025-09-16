b311fc5afd895e5edf76372654458d5d2733681b
Skilletpan <skilletpan.14@gmail.com>
2025-09-16 08:51:26 +0200

    Env and README update

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,10 @@
 /** The environment variable values of the app. */
 export default Object.freeze({
   /** The display name of the app. */
-  APP_NAME: import.meta.env.VITE_APP_NAME || 'Siege Randomizer',
+  APP_NAME: import.meta.env.VITE_APP_NAME,
 
   /** The URL to the repository of the app. */
-  APP_REPOSITORY: import.meta.env.VITE_APP_REPOSITORY || 'https://github.com/Skilletpan/siege-randomizer',
+  APP_REPOSITORY: import.meta.env.VITE_APP_REPOSITORY,
 
   /** The version of the app. */
   APP_VERSION: import.meta.env.VITE_APP_VERSION
