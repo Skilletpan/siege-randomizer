15281a04a81e66c1504e6df9f430f84d3fe83ba8
Skilletpan <skilletpan.14@gmail.com>
2025-09-22 00:01:50 +0200

    Added sides

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,13 @@
 import Gadget from './siege/Gadget';
 import Level from './siege/Level';
 import Season from './siege/Season';
+import Side from './siege/Side';
 import Weapon from './siege/Weapon';
 
 export {
   Gadget,
   Level,
   Season,
+  Side,
   Weapon
 }
