af74535f47958ce17e7917bddf009831d821342a
Skilletpan <skilletpan.14@gmail.com>
2026-01-13 18:39:27 +0100

    Added forgotten store export

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,11 @@
 import useLoadingStore from './LoadingStore';
 import useModalStore from './ModalStore';
+import useSettingsStore from './SettingsStore';
 import useSiegeStore from './SiegeStore';
 
 export {
   useLoadingStore,
   useModalStore,
+  useSettingsStore,
   useSiegeStore
 }
